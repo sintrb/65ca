a4a4fc6b8a4196f771a23fc65a3af29da43e16e0
RobinTang <sintrb@gmail.com>
2014-05-06 02:04:29 +0800

    modify

--- src/functions.h
+++ src/functions.h
@@ -11,8 +11,10 @@ Robin 2014-05-05
 extern t_val curval;
 
 
-#define M_WRITEBYTE(_cmd) writeout(_cmd);writeout(curval);
-#define M_WRITEWORD(_cmd) writeout(_cmd);writeout(curval);writeout(curval>>8);
+#define M_WRITECMD(_cmd) writeout(_cmd);;
+#define M_WRITEBYTE(_cmd) M_WRITECMD(_cmd);writeout(curval);
+#define M_WRITEWORD(_cmd) M_WRITECMD(_cmd);writeout(curval);writeout(curval>>8);
+#define M_WRITEREL(_cmd) M_WRITEBYTE(_cmd)
 
 // write out
 void writeout(t_val v);
